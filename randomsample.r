3a5c18c809d2a854a654dbf787f7fca85f7551f0
Allen Riddell <abr@ariddell.org>
2011-07-15 15:18:10 -0400

    Reorganizing.

--- randomsample.r
+++ randomsample.r
@@ -5,6 +5,6 @@ n_novels = 2903 # 1800-1836 (Garside et al., with updates)
 # sample with replacement, for the record
 the_sample = sample(1:n_novels,replace=TRUE)
 
-write.table(the_sample,"randomsample_with_replacement.dat")
-write.table(unique(the_sample),"randomsample.dat")
+write.csv(the_sample,"randomsample_with_replacement.csv")
+write.csv(unique(the_sample),"randomsample.csv")
 
